21a2a9bc621ef96439c6cf42d2f608a05414ecdf
PoggersBoy <169110551+PoggersBoy@users.noreply.github.com>
2025-07-12 18:10:09 -0400

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-echo ("What is your modpack")
+#/Gathering Variables\
+echo ("What is the full name of your modpack directory?")
 read modpack
+
+echo ("How much ram do you want allocated to this server? Add G for gigabyte and M for megabyte")
+read ram
